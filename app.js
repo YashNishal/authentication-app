71a2ccc79ca54dc53c9f2a47fc058ccfe940cdf0
Yash Nishal <YASHNISHAL@GMAIL.COM>
2021-09-02 19:16:31 +0530

    Level 1

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require("mongoose");
+const app = express();
+ 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = mongoose.model("User",userSchema);
+
+
+app.get("/", (req,res)=> {
+    res.render("home",{});
+});
+
+app.route("/login")
+.get((req,res)=> {
+    res.render("login");
+})
+.post( (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email : username}, (er,foundUser) => {
+        if(er) {
+            console.log(er);
+            return;
+        }
+        if(foundUser) {
+            if(foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    });
+});
+
+
+app.route("/register")
+.get((req,res)=> {
+    res.render("register",{});
+})
+.post( (req, res) => {
+    const user = new User({
+       email : req.body.username,
+       password : req.body.password 
+    });
+
+    user.save( er => {
+        if(er) {
+            console.log(er);
+        } else {
+            res.render("secrets");
+            console.log("registered.");
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+  console.log(`Server started on port 3000`);
+});
